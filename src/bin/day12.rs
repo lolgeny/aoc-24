8066f8ac53fcc9d86bd12b54653a260db6742714
Joel Swedensky <joel@swedensky.com>
2024-12-19 19:20:58 +0000

    Union find day 12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,52 +1,41 @@
-use std::collections::{HashMap, HashSet};
-
 use aoc24::load_input;
 use itertools::Itertools;
+use partitions::PartitionVec;
 
 fn part1(inp: &str) -> usize {
-    let grid: HashMap<(isize, isize), char> = inp.lines().enumerate()
-        .map(|(i, l)| l.chars().enumerate()
-            .map(move |(j, c)| ((i as isize, j as isize), c)))
-        .flatten().collect();
+    let grid = inp.lines().map(|l| l.chars().collect_vec()).collect_vec();
 
-    let mut visited = HashSet::new();
-    let mut queue = vec![(0,0)];
-    let mut neighbours = vec![];
-    let mut a = 0usize;
-    let mut p = 0;
+    let mut groups = PartitionVec::new();
+    let x_len = grid[0].len(); let y_len = grid.len();
+    for y in 0..y_len {
+        for x in 0..x_len {
+            groups.push((x,y));
+            let c = grid[y][x];
+            if x > 0 && grid[y][x-1] == c {groups.union(y*x_len+x, y*x_len+x-1);}
+            if y > 0 && grid[y-1][x] == c {groups.union(y*x_len+x, (y-1)*x_len+x);}
+        }
+    }
+    
     let mut total = 0;
-    while let Some(x) = queue.pop() {
-        if visited.contains(&x) {continue}
-        let g = *grid.get(&x).unwrap();
-        neighbours.push(x);
-        while let Some(n @ (i, j)) = neighbours.pop() {
-            if visited.contains(&n) {continue};
-            match grid.get(&n) {
-                Some(&l) if l == g => {},
-                Some(_) => {queue.push(n); continue}
-                None => continue
-            };
-            visited.insert(n);
-            a += 1;
-            // 845
-            // 1 3
-            // 726
+    for g in groups.all_sets() {
+        let mut area = 0;
+        let mut corners = 0;
+        for (_, &(x, y)) in g {
+            let c = grid[y][x];
+            let x = x as isize; let y = y as isize;
+            area += 1;
             let (e1, e2, e3, e4, e5, e6, e7, e8) =
-                [(i-1,j),(i,j-1),(i+1,j),(i,j+1), (i+1,j+1),(i+1,j-1),(i-1,j-1),(i-1,j+1)]
-                .into_iter().map(|(i2, j2)| grid.get(&(i2,j2))
-                .map_or(true, |x| *x != g))
+                [(x-1,y),(x,y-1),(x+1,y),(x,y+1), (x+1,y+1),(x+1,y-1),(x-1,y-1),(x-1,y+1)]
+                .into_iter()
+                .map(|(x2, y2)| if x2 >= 0 && y2 >= 0 && x2 < x_len as isize && y2 < y_len as isize {
+                    grid[y2 as usize][x2 as usize] != c
+                } else {true})
                 .collect_tuple().unwrap();
-            p += [(e1, e2, e7), (e2, e3, e6), (e3, e4, e5), (e4, e1, e8)].into_iter()
+            corners += [(e1, e2, e7), (e2, e3, e6), (e3, e4, e5), (e4, e1, e8)].into_iter()
                 .filter(|&(a, b, c)| (a && b) || (!a && !b && c))
                 .count();
-            neighbours.push((i+1,j));
-            neighbours.push((i-1,j));
-            neighbours.push((i,j+1));
-            neighbours.push((i,j-1));
         }
-        total += a*p;
-        a = 0;
-        p = 0;
+        total += area*corners;
     }
 
     total
